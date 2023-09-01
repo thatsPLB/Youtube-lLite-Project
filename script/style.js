93ca4f6554a635df420f24ec18fc109dc4eb648d
Plabon Saha <sahaplabon66@gmail.com>
2023-09-01 14:28:53 +0600

    third commit

--- script/style.js
+++ script/style.js
@@ -18,31 +18,32 @@ const handleCategory = async() =>{
 
 // Id
 const loadId = async(category_id) => {
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
-    const data = await response.json();
-    // console.log(data);
-// card-container section
-    const cardContainer = document.getElementById('card-container')
-    data.forEach((name) =>{
-        console.log(name);
-        const div = document.createElement('div')   
-        div.innerHTML = ` <div class="card w-96 bg-base-100 shadow-xl">
-        <figure class="px-10 pt-10">
-          <img src=${name?.profile_picture}
-        <div class="card-body items-center text-center">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-        
-        `
-        cardContainer.appendChild(div);
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
+  const data = await response.json();
+  const newData =data.data
+  console.log(data);
+  // card-container section
+  const cardContainer = document.getElementById('card-container')
+  newData.forEach((name) =>{
+    console.log(name);
+    const div = document.createElement('div')
+    div.classList = `card w-96 bg-base-100 shadow-xl`   
+    div.innerHTML = ` 
+    <figure class="px-10 pt-10">
+    <img src="${name.thumbnail}" alt="Shoes" class="rounded-xl" />
+    </figure>
+    <div class="card-body items-center text-center">
+    <h2 class="card-title">${name.title}</h2>
+    
+    <p>${name.others.views}</p>
+    <div class="card-actions">
+    </div>
+  </div>
+  `;
+  cardContainer.appendChild(div);
     });
 };
-
+console.log(loadId(1000));
 
 
 

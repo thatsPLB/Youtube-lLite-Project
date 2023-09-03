dae089d2c9870aa4dc078684d2f2a240ae2d6dca
Plabon Saha <sahaplabon66@gmail.com>
2023-09-03 20:09:02 +0600

    fourh commit

--- script/style.js
+++ script/style.js
@@ -10,7 +10,7 @@ const getCatagories = async () => {
       let button = document.createElement("button");
 
       button.innerHTML = `
-        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded" onclick="loadId('${data?.category_id}');" >${data.category }</button>`;
+        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded" onclick="loadId('${data?.category_id}');" >${data.category}</button>`;
       container.appendChild(button);
     });
   } catch (error) {
@@ -20,10 +20,10 @@ const getCatagories = async () => {
 
 getCatagories();
 
-let catId = 1000
+let catId = 1000;
 let doSort = false;
 const loadId = async (category_id, s = false) => {
-  category_id === undefined ? catId = catId : catId = category_id;
+  category_id === undefined ? (catId = catId) : (catId = category_id);
   doSort = s;
   const response = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${catId}`
@@ -31,43 +31,59 @@ const loadId = async (category_id, s = false) => {
   const data = await response.json();
   const newData = data.data;
   if (doSort) {
-      newData.forEach((item) => {
-        item.others.views =
-          parseFloat(item.others.views.replace("K", "")) * 1000;
-      });
+    newData.forEach((item) => {
+      item.others.views = parseFloat(item.others.views.replace("K", "")) * 1000;
+    });
 
-      var byView = newData.slice(0);
-      byView.sort((a, b) => {
-        return a.others.views - b.others.views;
-      });
+    var byView = newData.slice(0);
+    byView.sort((a, b) => {
+      return a.others.views - b.others.views;
+    });
 
-      byView.forEach((item) => {
-        const viewsInK = item.others.views / 1000;
-        item.others.views = viewsInK.toFixed(1) + "K";
-      });
-    }
+    byView.forEach((item) => {
+      const viewsInK = item.others.views / 1000;
+      item.others.views = viewsInK.toFixed(1) + "K";
+    });
+  }
   // card-container section
-  console.log(byView)
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
-  (byView === undefined ? newData : byView).forEach((name) => {
-    const div = document.createElement("div");
-    div.classList = `card w-96 bg-base-100 shadow-xl`;
-    div.innerHTML = ` 
+  if (newData.length !== 0) {
+    (byView === undefined ? newData : byView).forEach((name) => {
+      const div = document.createElement("div");
+      div.classList = `card w-96 bg-base-100 shadow-xl`;
+      div.innerHTML = ` 
     <figure class="px-10 pt-10">
     <img src="${name.thumbnail}" alt="Shoes" class="rounded-xl" />
     </figure>
-    <div class="card-body items-center text-center">
+    <div class="card-body items-center text-center ">
+      <div>
+
+    <img src="${name.authors[0].profile_picture}" style="width: 40px; height: 40px; border-radius: 50px"/>
+      </div>
+      <div>
+
     <h2 class="card-title">${name.title}</h2>
-    <img src="${name.authors?.profile_picture}/>
+    <p>${name.authors[0].profile_name}</p>
+    <p>${name.others.views} views</p>
+      </div>
     
-    <p>${name.others.views}</p>
-    <p>${name.others.posted_date}</p>
     <div class="card-actions">
     </div>
   </div>
   `;
+  console.log(newData)
+      cardContainer.appendChild(div);
+    });
+  } else {
+    const div = document.createElement("div");
+    div.classList = 'justify-center '
+    div.innerHTML = `
+    <img src='./images/Icon.png' />
+    <p>Oops!! Sorry, There is no content here</p>
+    `;
     cardContainer.appendChild(div);
-  });
+  }
 };
+
 loadId();
